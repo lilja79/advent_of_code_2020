f93b885ff33c8d72c36ac0f8efdcfdf812e6fba3
lilja79 <73644525+lilja79@users.noreply.github.com>
2020-12-04 10:47:09 +0100

    Update Dec1Spyder.py

--- Dec1Spyder.py
+++ Dec1Spyder.py
@@ -7,18 +7,9 @@ Created on Thu Dec  3 19:00:31 2020
 #%%
 with open('C:/Users/slb/Documents/AdventOfCode2020/Dec1.txt') as f:
     dec1_data = [int(i) for i in f]
-t = 0
-tt = 0
+
 for i in range(199):
     for j in range(i,200):
         if (dec1_data[i] + dec1_data[j]) == 2020:
             result = dec1_data[i] * dec1_data[j]
-            first_number = dec1_data[i]
-            second_number = dec1_data[j]
-            print(result)
-            print(first_number)
-            print(second_number)
-
-
-
-#%%
+print(result)
